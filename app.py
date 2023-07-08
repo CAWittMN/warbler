b9ee592910ff972fe20680782dcadf25a4856d4d
Casey Wittrock <caseywittrockmn@gmail.com>
2023-07-08 00:13:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ app = Flask(__name__)
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URL", "postgresql:///warbler"
+    "SQLALCHEMY_DATABASE_URL", "postgresql:///warbler"
 )
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_ECHO"] = False
-app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
+# app.config["SQLALCHEMY_ECHO"] = False
+# app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "it's a secret")
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
